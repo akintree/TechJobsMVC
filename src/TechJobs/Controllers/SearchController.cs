52187f7b5f412119879255891366796114139fd9
Annie Kintree <akintree53@gmail.com>
2020-03-19 17:47:48 -0500

    Main code for assignment 2

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,23 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
-
+        [HttpPost]
+        [Route("Search/Index")]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;          
+            if (searchType == "all")
+            {
+                ViewBag.searchResults = JobData.FindByValue(searchTerm);
+                return View("Index");
+            } else
+            { 
+                ViewBag.searchResults = JobData.FindByColumnAndValue(searchType, searchTerm);
+                return View("Index");
+            }
+            
+        }
     }
 }
+
+
